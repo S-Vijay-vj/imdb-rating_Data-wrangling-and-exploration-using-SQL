a38afa0d51c19fd3d88460940245a78e53db975b
S-Vijay-vj <112348575+S-Vijay-vj@users.noreply.github.com>
2023-02-11 18:56:37 +0530

    Update IMDB_rating_data_wrangling_&_exploration.sql
    Made some corrections and improvements in the exploration part.

--- IMDB_rating_data_wrangling_&_exploration.sql
+++ IMDB_rating_data_wrangling_&_exploration.sql
@@ -1,4 +1,4 @@
-------------------------------------------------------------------------
+-------------------------------------------------------------------------
 ------------------------------Data cleaning-----------------------------
 
 -- cleaning year column consist of data like (2018-2020 ,  2022– , III 2022)
@@ -104,98 +104,147 @@ ALTER TABLE imdb  ADD COLUMN weighted_rating FLOAT;
 
 update imdb 
 set weighted_rating = (votes / (votes + 25000.0)) * rating + (25000 / (votes + 25000.0)) * (select  AVG(rating) FROM imdb)
+
+
 				  
 ----------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------
 ------------------------------------ DATA EXPLORATION ----------------------------------------------
 				  
---- GETTING TOP RATED MOVIES AND SHOWS BY RATING ALONE.
+--- GETTING TOP 10 MOVIES AND SHOWS BY RATING ALONE.
 select * from imdb
-order by rating desc 
+order by rating desc
+limit 10;
 
---- GETTING TOP RATED MOVIES AND SHOWS BY VOTES ALONE.
+--- GETTING TOP 10 MOVIES AND SHOWS BY VOTES ALONE.
 select * from imdb
-order by votes desc 
+order by votes desc
+limit 10;
 
+--- GETTING TOP 10 MOVIES AND SHOWS BY WEIGHTED rating 
+select * from imdb 
+order by weighted_rating desc
+limit 10;
 
---- GETTING TOP RATED MOVIES AND SHOWS BY CONSIDERING RATING AND VOTES
- select * from imdb 
-order by rating desc , votes desc  
+--- GETTING DISTINCT GENRES
+select distinct genre 
+from imdb
+
+--- GETTING POPOULAR GENRES
+select genre, count(genre) as movies_per_genre from imdb
+group by genre
+order by movies_per_genre desc 
+limit 10;
+
+--- GETTING TOP RATED DRAMA MOVIES AND SHOWS
+select * from imdb  
+where genre like 'Drama'
+order by weighted_rating desc
+limit 10;
+
+--- GETTING TOP RATED DRAMA MOVIES AND SHOWS IN YEAR 2022
+select * from imdb  
+where genre like 'Drama' and year = 2022
+order by weighted_rating desc
+limit 10;
+
+--- GETTING TOP RATED 'COMEDY' MOVIES AND SHOWS
+select * from imdb  
+where genre like 'Comedy'
+order by weighted_rating desc
+limit 10;
+
+--- GETTING TOP RATED 'COMEDY' MOVIES AND SHOWS IN YEAR 2022
+select * from imdb  
+where genre like 'Comedy' and year = 2022
+order by weighted_rating desc 
+limit 10;
+
 
 --- GETTING LIST OF ACTION MOVIES
-select * from imdb i 
+select * from imdb  
 where genre like 'Action%'
 
-select distinct genre from imdb
+--- GETTING TOP 10 ACTION MOVIES AND SHOWS
+select * from imdb  
+where genre like 'Action%'
+order by weighted_rating desc
+limit 10;
 
 --- GETTING TOP RATED TV SHOWS
 select * from imdb i 
 where genre like '%Reality-TV%'
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10;
 
 --- GETTING TOP RATED ANIMATION MOVIES AND SHOWS
 select * from imdb i 
 where genre like '%Animation%'
-order by rating desc,votes desc
-limit 10;
-
-select * from imdb i 
-where votes > 100000
-order by rating desc--,votes desc 
+order by weighted_rating desc
+limit 10; 
 
 --- GETTING TOP RATED MOVIES AND SHOWS IN 2022
 select * from imdb i 
 where year = 2022
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10;
 
 --- GETTING TOP RATED MOVIES AND SHOWS IN 2023
 select * from imdb i 
 where year = 2023
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10;
 
---- GETTING MOST HYPED MOVIES SHOWS TO BE RELEALED IN 2024
+--- GETTING MOST HYPED MOVIES AND SHOWS GOING TO BE RELEASED IN 2024
 select * from imdb i 
 where year = 2024
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10;
 
---- GETTING MOST RATED MOVIES OR SHOWS FOR EACH YEAR
-select year,
+--- CHECKING FOR ANY MOVIES AND SHOWS TO BE RELEASED AFTER 2024 PRESENT IN THIS DATA
+select * from imdb i 
+where year > 2024;
+
+--- GETTING NUMBER OF MOVIES OR SHOWS PER  YEAR
+select year,COUNT(YEAR) as movies_per_year
 from imdb
 group by year
-order by year desc;
+order by movies_per_year desc;
 
 --- GETTING DISTINCT VALUES FROM CERTIFICATE COLUMN
 select distinct certificate  as "Types of certificate"
 from imdb
 
+--- GETTING NUMBER NUMBER OF MOVIES OR SHOWS PER CERTIFICATE
+select certificate ,COUNT(certificate) as movies_per_certificate
+from imdb
+group by certificate 
+order by movies_per_certificate desc;
+
 --- GETTING MOST RATED 'U' CERTIFICATE MOVIE AND SHOWS
 select * 
 from imdb 
 where certificate = 'U'
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10
 
 --- GETTING MOST RATED 'U' CERTIFICATE MOVIE AND SHOWS OF 2022
 select * 
 from imdb 
 where certificate = 'U' and "year" = 2022
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10
 
 --- GETTING MOST RATED 'UA' CERTIFICATE MOVIE AND SHOWS
 select * 
 from imdb 
 where certificate = 'UA'
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10
 
 --- GETTING MOST RATED 'UA' CERTIFICATE MOVIE AND SHOWS OF 2022
 select * 
 from imdb 
 where certificate = 'UA' and "year" = 2022
-order by rating desc,votes desc
+order by weighted_rating desc
 limit 10
